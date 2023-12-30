056c94f5fb830063a3b2009181833b24ce82b988
RaZzV2 <razzrozo10@gmail.com>
2023-12-30 16:39:12 +0200

    removed redundant functions

--- TPD_COMMON/src/main/java/ddg501/Book.java
+++ TPD_COMMON/src/main/java/ddg501/Book.java
@@ -45,15 +45,6 @@ public class Book implements Serializable {
     public Set<User> getUsers() {
         return users;
     }
-
-    public void addUser(User user) {
-        this.users.add(user);
-    }
-
-    public void removeUser(User user) {
-        this.users.remove(user);
-    }
-
     public long getId() {
         return id;
     }
